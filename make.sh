25232536e130e99e8eee018022e2ddf083b14915
Albi-n-ps <preextrasensory6@gmail.com>
2021-06-28 15:06:16 +0530

    you can do anything if you can do something

--- make.sh
+++ make.sh
@@ -1,9 +1,12 @@
 
 sudo apt-get install neovim
+sudo apt install npm
+sudo apt-get install nodejs
 
 rm -rf ~/.config/nvim
 
 mkdir ~/.config/nvim
 cp nvim ~/.config/ -rf
+npm install --global yarn
 cd ~/
 nvim			
